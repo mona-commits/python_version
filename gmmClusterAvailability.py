fe3164c03ada2b72e86b7a93b4adacc66581d53a
Mona <49145298+mona-commits@users.noreply.github.com>
2020-08-08 10:53:01 +0800

    updated

--- gmmClusterAvailability.py
+++ gmmClusterAvailability.py
@@ -1,25 +1,27 @@
+''' Defining function cluster availability '''
+
+import math
+import numpy as np
 def gmmClusterAvailability(gmm, gmmWinner):
     nGmm = gmm.k
 
-    sigmaWinner = sqrt(gmm.TrackerS(gmmWinner, mslice[:]))
-    miu_plus_sigma_winner = gmm.TrackerC(gmmWinner, mslice[:]) + sigmaWinner
-    miu_mins_sigma_winner = gmm.TrackerC(gmmWinner, mslice[:]) - sigmaWinner
+    sigmaWinner = math.sqrt(gmm.TrackerS[gmmWinner,:])   # Assuming the matrix is in the form of a numpy array
+    miu_plus_sigma_winner = gmm.TrackerC[gmmWinner,:] + sigmaWinner  # Assuming the matrix is in the form of a numpy array
+    miu_mins_sigma_winner = gmm.TrackerC[gmmWinner, :] - sigmaWinner # Assuming the matrix is in the form of a numpy array
 
-    totalProportion = length(gmm.TrackerC(gmmWinner, mslice[:])) * (gmm.k - 1)
+    totalProportion = length(gmm.TrackerC[gmmWinner,:]) * (gmm.k - 1)
     numInside = 0
 
     # count how many centers inside the winning cluster
-    for iGmm in mslice[1:nGmm]:
+    for iGmm in range(1,nGMM):
         if iGmm != gmmWinner:
-            center = gmm.TrackerC(iGmm, mslice[:])
+            center = gmm.TrackerC[iGmm,:]
             lessThanSigma = center < miu_plus_sigma_winner
             moreThanSigma = center > miu_mins_sigma_winner
-            insideWinner = lessThanSigma *elmul* moreThanSigma
+            insideWinner = np.multiply(lessThanSigma,moreThanSigma)
             numInside = numInside + sum(insideWinner)
-        end
-    end
-
     numOutside = abs(totalProportion - numInside)
-    rho = numOutside / totalProportion# eqn 14
+    rho = numOutside / totalProportion  # eqn 14
     if rho == 0:
         rho = 0.1    # minimum vigilance parameter
+    return rho
